ed1a1079b3fd145f16051fd63a6be31396403715
jtnguyen45 <jtnguyen45@gmail.com>
2024-04-04 17:02:27 -0700

    Add Google OAuth

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('home', { title: 'Stardew Valley Tracker' });
+});
+
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    scope: ['profile', 'email'],
+  }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/bundles',
+    failureRedirect: '/bundles'
+  }
+));
+
+router.get('/logout', function(req, res){
+  req.logout(function() {
+    res.redirect('/bundles');
+  });
 });
 
 module.exports = router;
